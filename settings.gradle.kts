be3aa0d113a0d4eaeaa5e2d386583b4c960c4fae
Jay Bobzin <standup@jaybobzin.com>
2023-11-14 16:59:17 -0500

    auth data module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,9 +35,11 @@ dependencyResolutionManagement {
 rootProject.name = "standup_nowin"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 include(":standup:app")
 include(":standup:integration:youtube")
 include(":standup:integration:openid")
+include(":standup:data:auth")
 
 include(":app")
 include(":app-nia-catalog")
